cd0c19a4a7bb4479f77e48480f8215d388aa9aef
Sarah Andyshak <sarah.andyshak@gmail.com>
2023-01-18 09:56:55 -0800

    Add test for multiple words in README.md, update corresponding code in scripts.js.

--- js/scripts.js
+++ js/scripts.js
@@ -16,8 +16,12 @@ function wordCounter(text) {
 }
 
 function numberOfOccurrencesInText(word,text) {
-  if (word === text) {
+  const textArray = text.split(" ");
+  let wordCount = 0;
+  textArray.forEach(function(element) {
+    if (word === text) {
     return 1;
-  }
+    }
+  });
   return 0;
 }
