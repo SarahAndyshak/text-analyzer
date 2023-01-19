f59d1975787a86d79295a88e0938699a9356ceb0
Sarah Andyshak <sarah.andyshak@gmail.com>
2023-01-18 20:02:08 -0800

    Update index.html and scripts.js to show final code from lessons, commented out process code in scripts.js to keep accessible.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,83 @@
+// Commented out code process, see bottom for updated code from lesson
+// // Business Logic
+
+// function wordCounter(text) {
+//   if (text.trim().length === 0) {
+//     return 0;
+//   }
+//   let wordCount = 0;
+//   const textArray = text.split(" ");
+//   textArray.forEach(function(element) {
+//     if (!Number(element)) {
+//       wordCount++;
+//     }
+//   });
+//   return wordCount;
+// }
+
+// //Had to add if(word.trim)... below in order to solve bug re: empty strings 
+// function numberOfOccurrencesInText(word, text) {
+//   if (word.trim().length === 0) {
+//     return 0;
+//   }
+//   const textArray = text.split(" ");
+//   let wordCount = 0;
+//   textArray.forEach(function(element) {
+//     if (element.toLowerCase().includes(word.toLowerCase())) {
+//       wordCount++;
+//     }
+//   });
+//   return wordCount;
+// }
+
+// // UI Logic -- below, even.preventDefault() is crossed out?
+
+// function handleFormSubmission() {
+//   event.preventDefault();
+//   const passage = document.getElementById("text-passage").value;
+//   const word = document.getElementById("word").value;
+//   const wordCount = wordCounter(passage);
+//   const occurrencesOfWord = numberOfOccurrencesInText(word, passage);
+//   document.getElementById("total-count").innerText = wordCount;
+//   document.getElementById("selected-count").innerText = occurrencesOfWord;
+// }
+
+// window.addEventListener("load", function() {
+//   document.querySelector("form#word-counter").addEventListener("submit", handleFormSubmission);
+// });
+
+// //Adding function to create bold passage, UI logic
+// function boldPassage(word, text) {
+//   if ((text.trim().length === 0) || (word.trim().length === 0)) {
+//     return null;
+//   }
+//   const p = document.createElement("p");
+//   let textArray = text.split(" ");
+//   textArray.forEach(function(element, index) {
+//     if (word === element) {
+//       const bold = document.createElement("strong");
+//       bold.append(element);
+//       p.append(bold);
+//     } else {
+//       p.append(element);
+//     }
+//     if (index !== (textArray.length - 1)) {
+//       p.append(" ");
+//     }
+//   });
+//   return p;
+// }
+
+// Utility Logic
+
+function isEmpty(testString) {
+  return (testString.trim().length === 0);
+}
+
 // Business Logic
 
 function wordCounter(text) {
-  if (text.trim().length === 0) {
+  if (isEmpty(text)) {
     return 0;
   }
   let wordCount = 0;
@@ -14,9 +90,8 @@ function wordCounter(text) {
   return wordCount;
 }
 
-//Had to hadd if(word.trim)... below in order to solve bug re: empty strings 
 function numberOfOccurrencesInText(word, text) {
-  if (word.trim().length === 0) {
+  if (isEmpty(word)) {
     return 0;
   }
   const textArray = text.split(" ");
@@ -29,25 +104,10 @@ function numberOfOccurrencesInText(word, text) {
   return wordCount;
 }
 
-// UI Logic -- below, even.preventDefault() is crossed out?
+// UI Logic
 
-function handleFormSubmission() {
-  event.preventDefault();
-  const passage = document.getElementById("text-passage").value;
-  const word = document.getElementById("word").value;
-  const wordCount = wordCounter(passage);
-  const occurrencesOfWord = numberOfOccurrencesInText(word, passage);
-  document.getElementById("total-count").innerText = wordCount;
-  document.getElementById("selected-count").innerText = occurrencesOfWord;
-}
-
-window.addEventListener("load", function() {
-  document.querySelector("form#word-counter").addEventListener("submit", handleFormSubmission);
-});
-
-//Adding function to create bold passage
 function boldPassage(word, text) {
-  if ((text.trim().length === 0) || (word.trim().length === 0)) {
+  if (isEmpty(word) || isEmpty(text)) {
     return null;
   }
   const p = document.createElement("p");
@@ -66,3 +126,23 @@ function boldPassage(word, text) {
   });
   return p;
 }
+
+function handleFormSubmission() {
+  event.preventDefault();
+  const passage = document.getElementById("text-passage").value;
+  const word = document.getElementById("word").value;
+  const wordCount = wordCounter(passage);
+  const occurrencesOfWord = numberOfOccurrencesInText(word, passage);
+  document.getElementById("total-count").innerText = wordCount;
+  document.getElementById("selected-count").innerText = occurrencesOfWord;
+  let boldedPassage = boldPassage(word, passage);
+  if (boldedPassage) {
+    document.querySelector("div#bolded-passage").append(boldedPassage);
+  } else {
+    document.querySelector("div#bolded-passage").innerText = null;
+  }
+}
+
+window.addEventListener("load", function() {
+  document.querySelector("form#word-counter").addEventListener("submit", handleFormSubmission);
+});
